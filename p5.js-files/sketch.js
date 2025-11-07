4bbb327f1d152a1a82f83550fee1d86ea7290248
Michael K <michaelkho@g.ucla.edu>
2025-11-07 13:37:00 -0800

    Rotating planets, smooth size transition

--- p5.js-files/sketch.js
+++ p5.js-files/sketch.js
@@ -1,14 +1,15 @@
 // TO BE REPLACED IN WEB EDITOR
 
 // CONFIGURATION
-const orbitRadius = 200; // How far planets are from the center
+const orbitRadius = 200;  // How far planets are from the center
 const minPlanetSize = 10; // The size of the least common interval
-const maxPlanetSize = 80 // The size of the most common interval
+const maxPlanetSize = 80  // The size of the most common interval
 
 // GLOBAL VARIABLES
 let dastgahSelector;
 let currentDastgahId;
 let processedData = [];
+let angleOffset = 0;      // Global offset of entire galaxy
 
 function setup() {
     createCanvas(600, 600);
@@ -16,18 +17,20 @@ function setup() {
     dastgahSelector = select('#dastgahSelector');
 
     const dastgahIds = Object.keys(fullData).sort();
-
     for (const id of dastgahIds) {
         dastgahSelector.option(id);
     }
 
     currentDastgahId = dastgahIds[0];
     dastgahSelector.changed(changeDastgah); // When user selects new option
-    recalculateGalaxy();
+    initializeGalaxy();
 }
 
 function draw() {
     background(20, 20, 30);
+
+    // Slow orbiting effect
+    angleOffset += 0.005;
 
     // Draw the Center Star (Dastgah Name)
     fill(255, 220, 150);
@@ -40,21 +43,24 @@ function draw() {
     translate(width / 2, height / 2);
 
     for (const planet of processedData) {
+        // Smooth transition for planet size
+        planet.currentSize = lerp(planet.currentSize, planet.targetSize, 0.05);
+
         push();
-        rotate(planet.angle);
+        rotate(planet.angle + angleOffset);
         const x = orbitRadius;
         const y = 0;
 
         // Draw the planet
         fill(150, 180, 255, 200);
         stroke(255);
-        ellipse(x, y, planet.size, planet.size);
+        ellipse(x, y, planet.currentSize, planet.currentSize);
 
         // Draw the planet's label
         fill(255);
         noStroke();
         textSize(14);
-        text(planet.name, x + planet.size / 2 + 10, y); // Place text next to planet
+        text(planet.name, x + planet.currentSize / 2 + 10, y); // Place text next to planet
 
         pop();
     }
@@ -66,8 +72,24 @@ function changeDastgah() {
     recalculateGalaxy();
 }
 
+// Creates the planet objects but sets their initial size to 0.
+function initializeGalaxy() {
+    const intervals = fullData[currentDastgahId];
+    const totalIntervals = Object.keys(intervals).length;
+    let index = 0;
+    for (const intervalName in intervals) {
+        processedData.push({
+            name: intervalName,
+            angle: map(index, 0, totalIntervals, 0, TWO_PI),
+            currentSize: 0,
+            targetSize: 0 // Until calculated
+        });
+        index++;
+    }
+    recalculateGalaxy();
+}
+
 function recalculateGalaxy() {
-    processedData = [];
     const intervals = fullData[currentDastgahId];
 
     let maxCount = 0;
@@ -77,20 +99,10 @@ function recalculateGalaxy() {
         }
     }
 
-    let index = 0;
-    const totalIntervals = Object.keys(intervals).length;
-    for (const intervalName in intervals) {
-        const count = intervals[intervalName];
-        const planetSize = map(count, 0, maxCount, minPlanetSize, maxPlanetSize);
-        const angle = map(index, 0, totalIntervals, 0, TWO_PI);
-
-        processedData.push({
-            name: intervalName,
-            size: planetSize,
-            angle: angle
-        });
-
-        index++;
+    // Update each planets targetSize
+    for(const planet of processedData) {
+        const count = intervals[planet.name] || 0;
+        planet.targetSize = map(count, 0, maxCount, minPlanetSize, maxPlanetSize);
     }
 }
 
